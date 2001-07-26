e2c78eb43c50ab1eca767313c7d938bcc2417407
twf <>
2001-07-26 13:55:41 +0000

    Fix compile error on SunOS

--- mdsplus/mdsshr/librtl.c
+++ mdsplus/mdsshr/librtl.c
@@ -9,7 +9,7 @@
 #include <mds_stdarg.h>
 #include <librtl_messages.h>
 
-static char *cvsrev = "@(#)$RCSfile: librtl.c,v $ $Revision: 1.88 $ $Date: 2001/07/26 13:51:23 $";
+static char *cvsrev = "@(#)$RCSfile: librtl.c,v $ $Revision: 1.89 $ $Date: 2001/07/26 13:55:41 $";
 
 extern int MdsCopyDxXd();
 
@@ -468,10 +468,10 @@ void pthread_lock_global_np()
 {
   if (!Initialized)
   {
-#ifdef PTHREAD_MUTEX_INITIALIZER 
+#if defined(PTHREAD_MUTEX_INITIALIZER) && defined(PTHREAD_MUTEX_RECURSIVE) 
     pthread_mutexattr_t m_attr;
     pthread_mutexattr_init(&m_attr);
-    pthread_mutexattr_setkind_np(&m_attr,PTHREAD_MUTEX_RECURSIVE_NP);
+    pthread_mutexattr_setkind_np(&m_attr,PTHREAD_MUTEX_RECURSIVE);
     pthread_mutex_init(&GlobalMutex,&m_attr);
 #else 
     pthread_mutex_init(&GlobalMutex,pthread_mutexattr_default);
