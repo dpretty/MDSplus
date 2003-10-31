33c1f674cb6f989ca8cdcf5cacf7258fab7cb057
jgk <>
2003-10-31 09:41:40 +0000

    Removed another code fragment and comment that are not longer relevant.

--- mdsplus/javascope/TWUFetchOptions.java
+++ mdsplus/javascope/TWUFetchOptions.java
@@ -11,7 +11,7 @@
 // Written by Marco van de Giessen <A.P.M.vandeGiessen@phys.uu.nl>, originally as a nested 
 // class in TwuDataProvider.java, now a proper 'first class' class in its own source file.
 //
-// $Id: TWUFetchOptions.java,v 2.3 2003/10/30 18:27:54 jgk Exp $
+// $Id: TWUFetchOptions.java,v 2.4 2003/10/31 09:41:40 jgk Exp $
 // 
 // For the modification history, see the CVS log.
 // -------------------------------------------------------------------------------------------------
@@ -46,16 +46,6 @@ class TWUFetchOptions
         if (start < 0 ) start = 0 ;
         if (step  < 1)  step  = 1 ;
         if (total < 0)  total = length ; 
-
-        // note: How should we hanlde total==0 ? it *may* not be
-        // a good idea to have 'total==0' translated to 'get all'.
-        // MvdG
-        //
-        if (total ==0)  total = length ; 
-        //
-        // Indeed, I like to think about total==0 as a legal 
-        // request for no data.  JGK
-        // (But other codes break on that idea for the moment.)
 
         final int requestedEnd  = start + (total-1)*step ;
 
@@ -104,5 +94,5 @@ class TWUFetchOptions
 }
 
 // -------------------------------------------------------------------------------------------------
-// End of: $Id: TWUFetchOptions.java,v 2.3 2003/10/30 18:27:54 jgk Exp $
+// End of: $Id: TWUFetchOptions.java,v 2.4 2003/10/31 09:41:40 jgk Exp $
 // -------------------------------------------------------------------------------------------------
