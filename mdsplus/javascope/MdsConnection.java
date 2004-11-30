043fcf4aec821627cd3e94c8d69c43c7a327c385
manduchi <>
2004-11-30 16:06:11 +0000

    Fix Race condition creating multiple MRT threads

--- mdsplus/javascope/MdsConnection.java
+++ mdsplus/javascope/MdsConnection.java
@@ -1,4 +1,4 @@
-/* $Id: MdsConnection.java,v 1.35 2004/10/08 13:32:58 manduchi Exp $ */
+/* $Id: MdsConnection.java,v 1.36 2004/11/30 16:06:11 manduchi Exp $ */
 import java.io.*;
 import java.net.*;
 import java.awt.*;
@@ -69,8 +69,8 @@ public class MdsConnection
 	class MRT extends Thread // Mds Receive Thread
 	{
         MdsMessage message;
-        boolean    kill = false;
         boolean    pending = false;
+        boolean    killed = false;
 
 	    public void run()
 	    {
@@ -112,9 +112,21 @@ public class MdsConnection
 	                MdsConnection.this.dispatchConnectionEvent(ce);
 	                MdsConnection.this.NotifyMessage();
 	            }
+                    killed = true;
+                    synchronized(this)
+                    {
+                      notify();
+                    }
 	        }
 	    }
 
+            public synchronized void waitExited()
+            {
+              while(!killed)
+                try{
+                  wait();
+                }catch(InterruptedException exc){}
+            }
 
 	    public synchronized MdsMessage GetMessage()
 	    {
@@ -353,6 +365,7 @@ public class MdsConnection
                 connection_listener.removeAllElements();
 	        dos.close();
             dis.close();
+            receiveThread.waitExited();
             connected = false;
 	    }
 	    catch(IOException e)
