41dc6c6abb3dd2025e691e81ec11289c613e6d73
twf <>
2006-04-26 18:37:43 +0000

    fix int64 reference

--- mdsplus/tdishr/TdiSql.c
+++ mdsplus/tdishr/TdiSql.c
@@ -18,6 +18,10 @@
 	The SQL string sElEcT is a blob write. 
 	Ken Klare, LANL P-4	(c)1991,1992
 */
+#include <config.h>
+#if SIZEOF__INT64 != 8
+typedef unsigned long long _int64;
+#endif
 #define _MOVC3(a,b,c) memcpy(c,b,a)
 #include <STATICdef.h>
 #include <string.h>
@@ -44,7 +48,7 @@
 #define  DTYPE_D DTYPE_G
 #endif
 
-STATIC_CONSTANT char *cvsrev = "@(#)$RCSfile: TdiSql.c,v $ $Revision: 1.22 $ $Date: 2006/04/24 14:32:19 $";
+STATIC_CONSTANT char *cvsrev = "@(#)$RCSfile: TdiSql.c,v $ $Revision: 1.23 $ $Date: 2006/04/26 18:37:43 $";
 
 extern int stat;
 extern int TdiFindImageSymbol();
@@ -80,7 +84,7 @@ STATIC_CONSTANT  unsigned int nan_f_bits =  0x7fbfffff;
 STATIC_CONSTANT unsigned long long nan_d_bits = 0xffffffff7ff7ffff;
 */
 STATIC_CONSTANT  unsigned int nan_f_bits =  0x7fc00000;
-STATIC_CONSTANT unsigned _int64 nan_d_bits = 0x7ff8000000000000;
+STATIC_CONSTANT _int64 nan_d_bits = 0x7ff8000000000000;
 
 static double d_null = 0;
 static float  f_null = 0;
