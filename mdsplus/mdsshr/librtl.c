e0afef5e895c7df6815dff621848271bf501cb9d
twf <>
2007-03-26 19:45:28 +0000

    Fix logic in LibTimeToVMSTime

--- mdsplus/mdsshr/librtl.c
+++ mdsplus/mdsshr/librtl.c
@@ -19,7 +19,7 @@
 #include <math.h>
 #include <STATICdef.h>
 
-STATIC_CONSTANT char *cvsrev = "@(#)$RCSfile: librtl.c,v $ $Revision: 1.163 $ $Date: 2007/03/21 14:32:04 $ $Name:  $";
+STATIC_CONSTANT char *cvsrev = "@(#)$RCSfile: librtl.c,v $ $Revision: 1.164 $ $Date: 2007/03/26 19:45:28 $ $Name:  $";
 
 extern int MdsCopyDxXd();
 STATIC_ROUTINE char *GetTdiLogical(char *name);
@@ -1483,7 +1483,6 @@ int LibTimeToVMSTime(time_t *time_in,_int64 *time_out) {
   _int64 addin = LONG_LONG_CONSTANT(0x7c95674beb4000);
   if (time_in == NULL)
     t=time(0);
-  else
 // _int64 addin = 0x7c95674beb4000;
 #if defined(USE_TM_GMTOFF)
   /* this is a suggestion to change all code 
