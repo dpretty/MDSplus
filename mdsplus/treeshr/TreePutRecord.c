f2bda4f27c4fcebe99c1e089501eab029ab31863
twf <>
2012-01-23 18:00:00 +0000

    Allow with_error for signals and numerics

--- mdsplus/treeshr/TreePutRecord.c
+++ mdsplus/treeshr/TreePutRecord.c
@@ -63,7 +63,7 @@ static int daylight = 0;
 #define LONG_LONG_CONSTANT(value) value##ll
 #endif
 
-static char *cvsrev = "@(#)$RCSfile: TreePutRecord.c,v $ $Revision: 1.92 $ $Date: 2010/08/04 19:01:11 $";
+static char *cvsrev = "@(#)$RCSfile: TreePutRecord.c,v $ $Revision: 1.93 $ $Date: 2012/01/23 18:00:00 $";
 
 #ifdef min
 #undef min
@@ -292,7 +292,8 @@ static int CheckUsage(PINO_DATABASE *dblist, NID *nid_ptr, NCI *nci)
     status = check(is_numeric ||
 		   (nci->dtype == DTYPE_PARAM) ||
 		   (nci->dtype == DTYPE_RANGE) ||
-		   (nci->dtype == DTYPE_WITH_UNITS) || is_expression);
+		   (nci->dtype == DTYPE_WITH_UNITS) || 
+                   (nci->dtype == DTYPE_WITH_ERROR) || is_expression);
     break;
    case TreeUSAGE_SIGNAL:
     status = check(is_numeric ||
@@ -300,7 +301,8 @@ static int CheckUsage(PINO_DATABASE *dblist, NID *nid_ptr, NCI *nci)
 		   (nci->dtype == DTYPE_DIMENSION) ||
 		   (nci->dtype == DTYPE_PARAM) ||
 		   (nci->dtype == DTYPE_RANGE) ||
-		   (nci->dtype == DTYPE_WITH_UNITS) || is_expression);
+		   (nci->dtype == DTYPE_WITH_UNITS) || 
+                   (nci->dtype == DTYPE_WITH_ERROR) || is_expression);
     break;
    case TreeUSAGE_TASK:
     status = check((nci->dtype == DTYPE_PROGRAM) ||
