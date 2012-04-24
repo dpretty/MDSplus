42e180eca7fb53e2616b6334714ad2ea7e5872c8
twf <>
2012-04-24 17:48:55 +0000

    Update from head to support solaris

--- mdsplus/tests/distribution_tests/test_all.py
+++ mdsplus/tests/distribution_tests/test_all.py
@@ -2,7 +2,6 @@ import sys
 
 def python_test():
   import MDSplus
-  import sys
   sys.path.insert(0,MDSplus.__path__[0])
   from unittest import TextTestRunner
   from MDSplus.tests import test_all
@@ -28,6 +27,27 @@ def test_debian(WORKSPACE,FLAVOR):
   if not ok:
     sys.exit(1)
  
+def test_solaris(WORKSPACE,FLAVOR):
+  from solaris_install import solaris_install_tests,install,remove
+  ok=False
+  try:
+    solaris_install_tests(WORKSPACE,FLAVOR)
+    sys.stdout.flush()
+    pkgs=('kernel','camac','mitdevices','python')
+    for pkg in pkgs:
+       install(pkg,FLAVOR)
+    sys.stdout.flush()
+    ok=python_test()
+    sys.stdout.flush()
+    pkgs=list(pkgs)
+    pkgs.reverse()
+    for pkg in pkgs:
+      remove(pkg,FLAVOR)
+  except Exception,e:
+    print "Error %s" % (e,)
+  if not ok:
+    sys.exit(1)
+
 def test_rpms(WORKSPACE,FLAVOR):
   from rpm_install import rpm_install_tests,rpm_install,rpm_remove,initYum,cleanYum
   ok=False
