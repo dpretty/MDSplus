57d37d9600d41db86deffbdd978e03b84153884b
twf <>
2013-04-25 14:27:43 +0000

    Fix error message

--- mdsplus/mdsobjects/python/wsgi/doScope.py
+++ mdsplus/mdsobjects/python/wsgi/doScope.py
@@ -358,7 +358,7 @@ def doScopepanel(self):
                     try:
                         x=makeData(sig.dim_of().data())
                     except Exception,e:
-                        response_headers.append(('ERROR'+sig_idx_s,'Error getting x axis of %s: "%s", error: %s' % (expr,e)))
+                        response_headers.append(('ERROR'+sig_idx_s,'Error getting x axis of: "%s", error: %s' % (expr,e)))
                         continue
                 response_headers.append(('X'+sig_idx_s+'_DATATYPE',x.__class__.__name__))
                 response_headers.append(('Y'+sig_idx_s+'_DATATYPE',y.__class__.__name__))
